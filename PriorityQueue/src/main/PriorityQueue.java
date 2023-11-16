ccd3d2d3741a3aa4091e9f4321bb924d99c349b2
Aniol0012 <aniol0012@gmail.com>
2023-11-16 10:50:48 +0100

    feat: set project structure

---
+++ PriorityQueue/src/main/PriorityQueue.java
@@ -0,0 +1,5 @@
+public class PriorityQueue {
+    public static void main(String[] args) {
+        System.out.println("Testing");
+    }
+}
---
+++ PriorityQueue/src/test/PriorityQueueTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PriorityQueueTest {
+    /*
+    @Test
+    void isEmpty_on_empty_stack_should_return_true() {
+        var emptyStack = new LinkedStack<Integer>();
+        assertTrue(emptyStack.isEmpty());
+    }
+    */
+}
