4d50270b5bf5052c9df2e15949a47bd8e691a2c6
Aniol Serrano <aniol0012@gmail.com>
2023-11-27 13:28:31 +0100

    tests: add new tests for Triplet

--- PriorityQueue/src/heaps/TripletTest.java
+++ PriorityQueue/src/heaps/TripletTest.java
@@ -1,14 +1,35 @@
 package heaps;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TripletTest {
     @Test
     void testSomeCompare() {
         var triplet1 = new HeapQueue.Triplet<Integer, Object>(1, 1L, new Object());
         var triplet2 = new HeapQueue.Triplet<Integer, Object>(2, 2L, new Object());
-        assertTrue(triplet2.compareTo(triplet1) > 0);
+        Assertions.assertTrue(triplet2.compareTo(triplet1) > 0);
+    }
+
+    @Test
+    void testPriorityCompare() {
+        var triplet1 = new HeapQueue.Triplet<Integer, Object>(1, 1L, new Object());
+        var triplet2 = new HeapQueue.Triplet<Integer, Object>(2, 2L, new Object());
+        Assertions.assertTrue(triplet1.compareTo(triplet2) < 0);
+    }
+
+    @Test
+    void testTimestampCompare() {
+        var triplet1 = new HeapQueue.Triplet<Integer, Object>(1, 2L, new Object());
+        var triplet2 = new HeapQueue.Triplet<Integer, Object>(1, 1L, new Object());
+        Assertions.assertTrue(triplet1.compareTo(triplet2) < 0);
+    }
+
+    @Test
+    void testEquality() {
+        var triplet1 = new HeapQueue.Triplet<Integer, Object>(1, 1L, new Object());
+        var triplet2 = new HeapQueue.Triplet<Integer, Object>(1, 1L, new Object());
+        assertEquals(0, triplet1.compareTo(triplet2));
     }
 }
